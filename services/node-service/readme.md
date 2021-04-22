94e21347c4a8e1a4ee81e2d6c466a3c6a22fff7b
Dhruv Patel <dhruv.m.patel@ecompliance.com>
2021-04-22 17:13:55 -0600

    feat: add swagger spec for node-service and enforce request response validation

--- services/node-service/readme.md
+++ services/node-service/readme.md
@@ -7,3 +7,7 @@ A basic node app running a server with an endpoint
 Run `copy .env.example .env` and in `.env` set PORT to 5000
 
 When you run `npm start` from the root, you will see API running at http://localhost:5000
+
+### Service contract validations
+
+To visualize request-response validations add `DEBUG=swagger-express-validator ` before start / start-dev commands
--- services/node-service/src/app.ts
+++ services/node-service/src/app.ts
@@ -2,6 +2,10 @@ import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import compression from 'compression';
 import cookieParser from 'cookie-parser';
+import fs from 'fs';
+import path from 'path';
+import SwaggerExpressValidator from 'swagger-express-validator';
+import yaml from 'js-yaml';
 import { sayHello } from './models/hello';
 
 const app: express.Application = express();
@@ -12,11 +16,22 @@ app.use(cors());
 app.use(compression());
 app.use(cookieParser());
 
+app.use(
+  SwaggerExpressValidator({
+    schema: yaml.load(
+      fs.readFileSync(path.join(__dirname, './api/spec.yaml'), 'utf8')
+    ) as string,
+    validateRequest: true,
+    validateResponse: true,
+    allowNullable: true,
+  })
+);
+
 app.get('/api/message', (req: Request, res: Response) => {
   res.json({ message: sayHello() });
 });
 
-app.get('/health', (req: Request, res: Response) => {
+app.get('/api/health', (req: Request, res: Response) => {
   res.status(200).json({ message: 'Server is healthy!' });
 });
 
