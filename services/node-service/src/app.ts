17e56618a00e87228cb1c7e7fae75249466bb3d8
Dhruv Patel <dhruv.m.patel@ecompliance.com>
2021-05-17 13:29:51 -0600

    feat: Add OpenAPI support with express-openapi-validator

--- services/node-service/src/app.ts
+++ services/node-service/src/app.ts
@@ -4,7 +4,7 @@ import compression from 'compression';
 import cookieParser from 'cookie-parser';
 import fs from 'fs';
 import path from 'path';
-import SwaggerExpressValidator from 'swagger-express-validator';
+import * as ExpressOpenApiValidator from 'express-openapi-validator';
 import yaml from 'js-yaml';
 import { sayHello } from './models/hello';
 
@@ -17,13 +17,12 @@ app.use(compression());
 app.use(cookieParser());
 
 app.use(
-  SwaggerExpressValidator({
-    schema: yaml.load(
+  ExpressOpenApiValidator.middleware({
+    apiSpec: yaml.load(
       fs.readFileSync(path.join(__dirname, './api/spec.yaml'), 'utf8')
     ) as string,
-    validateRequest: true,
-    validateResponse: true,
-    allowNullable: true,
+    validateRequests: true,
+    validateResponses: true,
   })
 );
 
@@ -40,7 +39,11 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     console.error(err.message, err.stack);
     res.status(500).send('Internal server error');
   } else {
-    next();
+    try {
+      next();
+    } catch (error) {
+      console.error(error.message, error.stack);
+    }
   }
 });
 
