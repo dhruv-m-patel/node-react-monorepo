71ea96431aa1d763a2ae47ecaf110272cacd8c63
Dhruv Patel <dhruv.codelab@gmail.com>
2021-08-14 03:23:23 -0600

    docs: minor updates

--- jest.config.js
+++ jest.config.js
@@ -4,18 +4,14 @@ module.exports = {
   ...baseConfig,
   projects: [
     '<rootDir>/packages/*/jest.config.js',
-    '<rootDir>/microservices/*/jest.config.js',
-    '<rootDir>/react-app/jest.config.js',
+    '<rootDir>/services/*/jest.config.js',
+    '<rootDir>/apps/*/jest.config.js',
   ],
-  roots: [
-    '<rootDir>/packages',
-    '<rootDir>/microservices',
-    '<rootDir>/react-app',
-  ],
+  roots: ['<rootDir>/packages', '<rootDir>/services', '<rootDir>/apps'],
   collectCoverageFrom: [
     'packages/**/*.{js,jsx,ts,tsx}',
-    'microservices/**/*.{js,jsx,ts,tsx}',
-    'react-app/**/*.{js,jsx,ts,tsx}',
+    'services/**/*.{js,jsx,ts,tsx}',
+    'apps/**/*.{js,jsx,ts,tsx}',
   ],
   moduleDirectories: ['node_modules'],
 };
