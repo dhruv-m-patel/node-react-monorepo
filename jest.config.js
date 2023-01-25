f94857a0dba403cbab55bcc3aa0dd24b0ee66299
Dhruv Patel <dpatel@gasbuddy.com>
2023-01-25 19:21:11 +0530

    chore: consolidate jest base config

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,24 @@
 const baseConfig = require('./jest.config.base');
 
 module.exports = {
-  ...baseConfig,
-  projects: [
-    '<rootDir>/packages/*/jest.config.js',
-    '<rootDir>/services/*/jest.config.js',
-    '<rootDir>/apps/*/jest.config.js',
-  ],
-  roots: ['<rootDir>/packages', '<rootDir>/services', '<rootDir>/apps'],
+  clearMocks: true,
   collectCoverageFrom: [
     'packages/**/*.{js,jsx,ts,tsx}',
     'services/**/*.{js,jsx,ts,tsx}',
     'apps/**/*.{js,jsx,ts,tsx}',
   ],
+  coverageReporters: ['json', 'text', 'lcov', 'clover'],
   moduleDirectories: ['node_modules'],
+  moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'node'],
+  projects: [
+    '<rootDir>/packages/*/jest.config.js',
+    '<rootDir>/services/*/jest.config.js',
+    '<rootDir>/apps/*/jest.config.js',
+  ],
+  rootDir: __dirname,
+  roots: ['<rootDir>/packages', '<rootDir>/services', '<rootDir>/apps'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  verbose: true,
 };
