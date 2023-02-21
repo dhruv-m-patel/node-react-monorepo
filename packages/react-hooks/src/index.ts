6fc17c8ddd68b6c0c43cc618040d597068acba12
Dhruv Patel <dhruv.codelab@gmail.com>
2023-02-21 14:55:20 +0530

    feat!: upgrade to lerna v6; start using yarn; cleanup and rebuild with yarn

--- packages/react-hooks/src/index.ts
+++ packages/react-hooks/src/index.ts
@@ -1,27 +1,12 @@
-import useStateFromInput from './useInputState';
-import useEffectWithDeepCompare from './useEffectWithDeepCompare';
-import useDebounce from './useDebounce';
-import useEventListener from './useEventListener';
-import useInterval from './useInterval';
-import useKeyPress from './useKeyPress';
-import usePreviousValue from './usePreviousValue';
-import useToggle from './useToggle';
-import useFetch from './useFetch';
-import useOnline from './useOnline';
-import useGeolocation from './useGeolocation';
-import useTimeout from './useTimeout';
-
-export {
-  useStateFromInput,
-  useEffectWithDeepCompare,
-  useDebounce,
-  useEventListener,
-  useInterval,
-  useKeyPress,
-  usePreviousValue,
-  useToggle,
-  useOnline,
-  useFetch,
-  useGeolocation,
-  useTimeout,
-};
+export * from './useInputState';
+export * from './useEffectWithDeepCompare';
+export * from './useDebounce';
+export * from './useEventListener';
+export * from './useInterval';
+export * from './useKeyPress';
+export * from './usePreviousValue';
+export * from './useToggle';
+export * from './useFetch';
+export * from './useOnline';
+export * from './useGeolocation';
+export * from './useTimeout';
