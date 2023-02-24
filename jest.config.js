9564d8831b4a50add2961e4831835c33c71271df
Dhruv Patel <dpatel@gasbuddy.com>
2023-02-24 19:23:57 +0530

    docs: cleanup configs and update readme to reflect repo name change

--- jest.config.js
+++ jest.config.js
@@ -2,19 +2,11 @@ const baseConfig = require('./jest.config');
 
 module.exports = {
   clearMocks: true,
-  collectCoverageFrom: [
-    'packages/**/*.{js,jsx,ts,tsx}',
-    'services/**/*.{js,jsx,ts,tsx}',
-    'apps/**/*.{js,jsx,ts,tsx}',
-  ],
+  collectCoverageFrom: ['packages/**/*.{js,jsx,ts,tsx}'],
   coverageReporters: ['json', 'text', 'lcov', 'clover'],
   moduleDirectories: ['node_modules'],
   moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'node'],
-  projects: [
-    '<rootDir>/packages/*/jest.config.js',
-    '<rootDir>/services/*/jest.config.js',
-    '<rootDir>/apps/*/jest.config.js',
-  ],
+  projects: ['<rootDir>/packages/*/jest.config.js'],
   rootDir: __dirname,
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
