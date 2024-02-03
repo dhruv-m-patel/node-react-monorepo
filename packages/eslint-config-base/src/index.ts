e5e3f00b1cea0b8b517844d51ee52cfa43f93724
Dhruv Patel <dhruv.codelab@gmail.com>
2024-02-03 17:05:40 -0600

    chore: Add eslint package for base linter configurations

---
+++ packages/eslint-config-base/src/index.ts
@@ -0,0 +1,36 @@
+import { ESLint } from "eslint";
+
+export default {
+  root: true,
+  extends: [
+    'airbnb-base',
+  ],
+  overrides: [
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      extends: [
+        'airbnb-base',
+        'airbnb-typescript/base',
+      ],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: './tsconfig.json'
+      },
+      plugins: ['@typescript-eslint'],
+    },
+    {
+      files: ['**/*.js', '**/*.jsx'],
+      extends: [
+        'airbnb-base',
+      ],
+      parser: '@babel/eslint-parser',
+      plugins: ['@babel'],
+    }
+  ],
+  rules: {
+    'import/prefer-default-export': [0],
+    'no-param-reassign': [2, {
+      props: false,
+    }],
+  },
+} as ESLint.ConfigData;
